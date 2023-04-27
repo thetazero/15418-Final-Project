1fc1c4f0b31cf16d4f32e2a06e33b3f2d6ed28fb
thetazero <lesha.seletskiy@gmail.com>
2023-04-27 10:31:10 -0400

    Add basic cuda program

---
+++ cuda/main.cu
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <assert.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+__global__ void test(){
+  printf("Hi Cuda World\n");
+}
+
+void print_gpu() {
+  int nDevices = 0;
+  cudaGetDeviceCount(&nDevices);
+  printf("Number of GPUs: %d\n", nDevices);
+  for (int i = 0; i < nDevices; i++) {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, i);
+    printf("Device Number: %d\n", i);
+    printf("  Device name: %s\n", prop.name);
+    printf("  Memory Clock Rate (KHz): %d\n",
+    prop.memoryClockRate);
+    printf("  Memory Bus Width (bits): %d\n",
+    prop.memoryBusWidth);
+    printf("  Peak Memory Bandwidth (GB/s): %f\n\n",
+    2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+  }
+}
+
+int main(int argc, char** argv )
+{
+   test<<<1,1>>>();
+   cudaDeviceSynchronize();
+   print_gpu();
+   return 0;
+}
