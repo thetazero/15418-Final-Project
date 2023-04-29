650f196bcadc19e04279698e5486a66ca8b705d4
thetazero <lesha.seletskiy@gmail.com>
2023-04-29 12:00:31 -0400

    Add scan horizontal test case

--- cuda/eval.cuh
+++ cuda/eval.cuh
@@ -1,56 +1,16 @@
 #ifndef EVAL
 #define EVAL
-__device__ __inline__ int idx(int r, int c, int size) { return r * size + c; }
+__device__ __inline__ 
+int idx(int r, int c, int size) { return r * size + c; }
 
-__inline__ void update_scratch(int state, int i, char *x_scratch, char *o_scratch) {
-  if (state == 0) return;
-  char *scratch = state > 0 ? x_scratch : o_scratch;
-  state = abs(state);
-  scratch[i] = max(scratch[i], state);
-}
+__device__ __inline__ 
+void update_scratch(int state, int i, char *x_scratch, char *o_scratch);
 
 __device__ __inline__
-int update_state(int r, int c, int size, int state, char *board, char *x_scratch, char *o_scratch) {
-  int i = idx(r, c, size);
-  if (board[i] == '.') {
-    update_scratch(state, i, x_scratch, o_scratch);
-  } else {
-    bool new_x = board[i] == 'x';
-    bool old_x = state >= 1;
-    int dir = new_x ? 1 : -1;
-    if (new_x != old_x) {
-      state = dir;
-    } else {
-      state += dir;
-    }
-  }
-}
-
-// Clearly not going to make functions for each direction, writing these to help myself think
-__device__ __inline__
-void scan_right(int size, char *board, char *x_scratch, char *o_scratch) {
-  int state = 0;
-  // 0 = empty, +n = n x's in a row, -n = n o's in a row
-  int r0 = 0, c0 = 0;
-  for (int r = r0; r < size; r++) {
-    for (int c = c0; c < size; c++) {
-      state = update_state(r, c, size, state, board, x_scratch, o_scratch);
-    }
-    state = 0;
-  }
-}
+int update_state(int r, int c, int size, int state, char *board, char *x_scratch, char *o_scratch);
 
 __device__ __inline__
-void scan_left(int size, char *board, char *x_scratch, char *o_scratch) {
-  int state = 0;
-  int r0 = 0, c0 = 0;
-  for (int c=c0; c < size; c++){
-    for (int r=r0; r < size; r++) {
-      state = update_state(r, c, size, state, board, x_scratch, o_scratch);
-    }
-    state = 0;
-  }
-}
+void scan_horizontal(int size, char *board, char *x_scratch, char *o_scratch);
 
 __global__
 void eval(int size, char *board, char *x_scratch, char *o_scratch) {
--- cuda/tests.cu
+++ cuda/tests.cu
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include "eval.cuh"
 #include <assert.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include "eval.cuh"
+#include <stdio.h>
 
 __global__ void idx_kernel(int *ans, int r, int c, int size) {
   *ans = idx(r, c, size);
 }
 
-int idx_wrapper(int r, int c, int size){
+int idx_wrapper(int r, int c, int size) {
   int h_ans;
   int *d_ans;
   cudaMalloc(&d_ans, sizeof(int));
@@ -18,12 +18,62 @@ int idx_wrapper(int r, int c, int size){
   return h_ans;
 }
 
+char *to_device(char *h_arr, int size) {
+  char *d_arr;
+  cudaMalloc(&d_arr, size);
+  cudaMemcpy(d_arr, h_arr, size, cudaMemcpyHostToDevice);
+  return d_arr;
+}
+
+char *to_host(char *d_arr, int size) {
+  char *h_arr = new char[size];
+  cudaMemcpy(h_arr, d_arr, size, cudaMemcpyDeviceToHost);
+  return h_arr;
+}
+
+void scan_horizontal_wrapper(int size, char *board, char *x_scratch,
+                             char *o_scratch, int dc) {
+  size_t board_mem_size = size * size * sizeof(char);
+  char *d_board = to_device(board, board_mem_size);
+  char *d_x_scratch = to_device(x_scratch, board_mem_size);
+  char *d_o_scratch = to_device(o_scratch, board_mem_size);
+  scan_horizontal<<<1, 1>>>(size, d_board, d_x_scratch, d_o_scratch, dc);
+  cudaMemcpy(board, d_board, board_mem_size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(x_scratch, d_x_scratch, board_mem_size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(o_scratch, d_o_scratch, board_mem_size, cudaMemcpyDeviceToHost);
+}
+
 void test_idx() {
   assert(idx_wrapper(1, 4, 5) == 9);
   assert(idx_wrapper(0, 0, 5) == 0);
   assert(idx_wrapper(0, 7, 12) == 7);
   assert(idx_wrapper(8, 3, 10) == 83);
   printf("idx_tests sucessful\n");
+}
+
+void test_scan_horizontal() {
+  char board[9] = {'x', 'x', '.', 
+                   '.', 'o', '.',
+                   'x', '.', 'o'};
+  char x_scratch[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char o_scratch[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  scan_horizontal_wrapper(3, board, x_scratch, o_scratch, 1);
+  char expected_board[9] = {'x', 'x', '.', 
+                            '.', 'o', '.',
+                            'x', '.', 'o'};
+  char expected_x_scratch[9] = {0, 0, 2, 
+                                0, 0, 0,
+                                0, 1, 0};
+  char expected_o_scratch[9] = {0, 0, 0, 
+                                0, 0, 1,
+                                0, 0, 0};
+  scan_horizontal_wrapper(3, board, x_scratch, o_scratch, 1);
+  for (int i = 0; i < 9; i++) {
+    assert(board[i] == expected_board[i]);
+    assert(x_scratch[i] == expected_x_scratch[i]);
+    assert(o_scratch[i] == expected_o_scratch[i]);
+  }
+  printf("scan_horizontal_tests sucessful\n");
 }
 
 int main() {
