4ef98247b4e07229df9b99cd810281784940c5db
thetazero <lesha.seletskiy@gmail.com>
2023-04-30 18:05:26 -0400

    Tets scan vertical

--- cuda/eval.cuh
+++ cuda/eval.cuh
@@ -49,4 +49,17 @@ void scan_horizontal(int size, char *board, char *x_scratch, char *o_scratch, in
   }
 }
 
+__device__ __inline__
+void scan_vertical(int size, char *board, char *x_scratch, char *o_scratch, int dr) {
+  int r0 = dr > 0 ? 0 : size-1;
+  int c0 = 0;
+  int r_terminate = dr > 0 ? size : -1;
+  for (int c = c0; c < size; c++) {
+    for (int r = r0; r != r_terminate; r+dr) {
+      state = update_state(r, c, size, state, board, x_scratch, o_scratch);
+    }
+    state = 0;
+  }
+}
+
 #endif
--- cuda/tests.cu
+++ cuda/tests.cu
@@ -49,19 +49,22 @@ void scan_horizontal_wrapper(int size, char *board, char *x_scratch,
   cudaMemcpy(o_scratch, d_o_scratch, board_mem_size, cudaMemcpyDeviceToHost);
 }
 
-void test_scan_horizontal_helper(string name, int size, char *board, char *x_scratch, char *o_scratch,
-                                 char *expected_board, char *expected_x_scratch,
-                                 char *expected_o_scratch, int dc) {
-  cout << "Running test: " << name << endl;
-  scan_horizontal_wrapper(size, board, x_scratch, o_scratch, dc);
-  for (int i = 0; i < size * size; i++) {
-    cout << "Got: " << board[i] << (int)x_scratch[i] << (int)o_scratch[i] << endl;
-    cout << "Expect: " << expected_board[i] << (int)expected_x_scratch[i] << (int)expected_o_scratch[i] << endl;
-    assert(board[i] == expected_board[i]);
-    assert(x_scratch[i] == expected_x_scratch[i]);
-    assert(o_scratch[i] == expected_o_scratch[i]);
-  }
+__global__  
+void scan_vertical_kernel(int size, char *board, char *x_scratch, char *o_scratch, int dr){
+  scan_vertical(size, board, x_scratch, o_scratch, dr);
 }
+
+void scan_vertical_wrapper(int size, char *board, char *x_scratch, char *o_scratch, int dr){
+  size_t board_mem_size = size * size * sizeof(char);
+  char *d_board = to_device(board, board_mem_size);
+  char *d_x_scratch = to_device(x_scratch, board_mem_size);
+  char *d_o_scratch = to_device(o_scratch, board_mem_size);
+  scan_vertical_kernel<<<1, 1>>>(size, d_board, d_x_scratch, d_o_scratch, dr);
+  cudaMemcpy(board, d_board, board_mem_size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(x_scratch, d_x_scratch, board_mem_size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(o_scratch, d_o_scratch, board_mem_size, cudaMemcpyDeviceToHost);
+}
+
 
 void test_idx() {
   assert(idx_wrapper(1, 4, 5) == 9);
@@ -70,6 +73,25 @@ void test_idx() {
   assert(idx_wrapper(8, 3, 10) == 83);
 }
 
+void test_scan_helper(string name, int size, char *board, char *x_scratch, char *o_scratch,
+                                 char *expected_board, char *expected_x_scratch,
+                                 char *expected_o_scratch, int dr, int dc) {
+  cout << "Running test: " << name << endl;
+  if (dr == 0) {
+    scan_horizontal_wrapper(size, board, x_scratch, o_scratch, dc);
+  } else if (dc == 0) {
+    scan_vertical_wrapper(size, board, x_scratch, o_scratch, dr);
+  } else {
+    assert(false);
+  }
+  for (int i = 0; i < size * size; i++) {
+    cout << "Got: " << board[i] << (int)x_scratch[i] << (int)o_scratch[i] << endl;
+    cout << "Expect: " << expected_board[i] << (int)expected_x_scratch[i] << (int)expected_o_scratch[i] << endl;
+    assert(board[i] == expected_board[i]);
+    assert(x_scratch[i] == expected_x_scratch[i]);
+    assert(o_scratch[i] == expected_o_scratch[i]);
+  }
+}
 
 void test_scan_horizontal() {
   char board[9] = {'x', 'x', '.',
@@ -86,19 +108,49 @@ void test_scan_horizontal() {
   char expected_o_scratch[9] = {0, 0, 0,
                                 0, 0, 1,
                                 0, 0, 0};
-  test_scan_horizontal_helper("scan right", 3, board, x_scratch, o_scratch,
-    expected_board, expected_x_scratch, expected_o_scratch, 1);
+  test_scan_helper("scan right", 3, board, x_scratch, o_scratch,
+    expected_board, expected_x_scratch, expected_o_scratch, 0, 1);
 
   char e_xs[9] = {0, 0, 2, 0, 0, 0, 0, 1, 0};
   char e_os[9] = {0, 0, 0, 1, 0, 1, 0, 1, 0};
   // scan same board with previous scratch but from left
-  test_scan_horizontal_helper("scan left", 3, board, x_scratch, o_scratch,
-    expected_board, e_xs, e_os, -1);
+  test_scan_helper("scan left", 3, board, x_scratch, o_scratch,
+    expected_board, e_xs, e_os, 0, -1);
+}
+
+void test_scan_vertical() {
+  char board[9] = {'x', 'x', '.',
+                   '.', 'o', '.',
+                   'x', '.', 'o'};
+  char x_scratch[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char o_scratch[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  char expected_board[9] = {'x', 'x', '.',
+                            '.', 'o', '.',
+                            'x', '.', 'o'};
+  char expected_x_scratch[9] = {0, 0, 0,
+                                1, 0, 0,
+                                0, 0, 0};
+  char expected_o_scratch[9] = {0, 0, 0,
+                                0, 0, 0,
+                                0, 1, 0};
+  test_scan_helper("scan down", 3, board, x_scratch, o_scratch,
+    expected_board, expected_x_scratch, expected_o_scratch, 1, 0);
+
+  char e_xs[9] = {0, 0, 0, 
+                  1, 0, 0, 
+                  0, 0, 0};
+  char e_os[9] = {0, 0, 0, 
+                  0, 0, 1, 
+                  0, 1, 0};
+  // scan same board with previous scratch but from left
+  test_scan_helper("scan up", 3, board, x_scratch, o_scratch,
+    expected_board, e_xs, e_os, -1, 0);
 
 }
 
 int main() {
   test_idx();
   test_scan_horizontal();
+  test_scan_vertical();
   return 0;
 }
